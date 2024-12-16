94cbfa4691b7bdda1126534839bf56a702287799
VoltrY <voltry6@gmail.com>
2024-12-16 13:27:55 +0300

    Hangman game in python

---
+++ wordlist.py
@@ -0,0 +1,12 @@
+
+words = (
+    "apple", "banana", "grape", "orange", "melon", "pear", "peach", "cherry", "plum", "mango",
+    "table", "chair", "desk", "lamp", "shelf", "sofa", "carpet", "pillow", "blanket", "drawer",
+    "river", "mountain", "valley", "ocean", "forest", "desert", "canyon", "island", "lake", "waterfall",
+    "happy", "sad", "angry", "excited", "scared", "bored", "calm", "curious", "tired", "nervous",
+    "run", "walk", "jump", "swim", "climb", "dance", "sing", "write", "read", "play",
+    "red", "blue", "green", "yellow", "purple", "orange", "black", "white", "pink", "brown",
+    "dog", "cat", "bird", "fish", "horse", "cow", "sheep", "rabbit", "mouse", "turtle",
+    "book", "pen", "paper", "notebook", "marker", "eraser", "pencil", "ruler", "calculator", "bag",
+    "cloud", "rain", "storm", "snow", "sun", "moon", "star", "wind", "fog", "lightning"
+)
